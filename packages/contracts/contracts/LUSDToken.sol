c94ece98c1333ee78db4c940b1e2708d3877740e
rjrunner44 <rob@adivate.net>
2021-07-09 14:18:46 -0700

    symbol rename - minimum changes

--- packages/contracts/contracts/LUSDToken.sol
+++ packages/contracts/contracts/LUSDToken.sol
@@ -28,8 +28,8 @@ contract LUSDToken is CheckContract, ILUSDToken {
     using SafeMath for uint256;
     
     uint256 private _totalSupply;
-    string constant internal _NAME = "LUSD Stablecoin";
-    string constant internal _SYMBOL = "LUSD";
+    string constant internal _NAME = "EEUR Stablecoin";
+    string constant internal _SYMBOL = "EEUR";
     string constant internal _VERSION = "1";
     uint8 constant internal _DECIMALS = 18;
     
