3e1492b2f2da78bfacab4ad7eacd0632394efa50
rjrunner44 <rob@adivate.net>
2021-08-27 12:38:08 -0700

    added contract diffs report

--- packages/contracts/diffs/readme.md
+++ packages/contracts/diffs/readme.md
@@ -1,7 +1,13 @@
 This directory is only used to diff contract files between what Liquity has in their repo, what they have live, and our contracts. This code is NOT run or used for anything else
 
-e.g.
+To compare Liquity live deployed contracts against what is in their repo:
 
 ```
 ./rundiffs.sh > diff-liquity-live-to-repo.log
+```
+
+To compare Liquity live deployed contracts against the ew versions in this repo:
+
+```
+./rundiffs-ew.sh > diff-liquity-live-ew-ew.log
 ```
---
+++ packages/contracts/diffs/rundiffs-ew.sh
@@ -0,0 +1,130 @@
+echo "************************************************************"
+echo "DIFF: ActivePool (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/ActivePool.sol ../contracts/ActivePool.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: BorrowerOperations (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/BorrowerOperations.sol ../contracts/BorrowerOperations.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: CollSurplusPool (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/CollSurplusPool.sol ../contracts/CollSurplusPool.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: CommunityIssuance (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/CommunityIssuance.sol ../contracts/LQTY/CommunityIssuance.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: DefaultPool (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/DefaultPool.sol ../contracts/DefaultPool.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: GasPool (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/GasPool.sol ../contracts/GasPool.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: HintHelpers (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/HintHelpers.sol ../contracts/HintHelpers.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: LockupContract (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/LockupContract.sol ../contracts/LQTY/LockupContract.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: LockupContractFactory (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/LockupContractFactory.sol ../contracts/LQTY/LockupContractFactory.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: LQTYStaking (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/LQTYStaking.sol ../contracts/LQTY/LQTYStaking.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: LQTYToken (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/LQTYToken.sol ../contracts/LQTY/LQTYToken.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: LUSDToken (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/LUSDToken.sol ../contracts/LUSDToken.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: MultiTroveGetter (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/MultiTroveGetter.sol ../contracts/MultiTroveGetter.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: PriceFeed (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/PriceFeed.sol ../contracts/PriceFeed.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: SortedTroves (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/SortedTroves.sol ../contracts/SortedTroves.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: StabilityPool (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/StabilityPool.sol ../contracts/StabilityPool.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: TellorCaller (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/TellorCaller.sol ../contracts/Dependencies/TellorCaller.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: TroveManager (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/TroveManager.sol ../contracts/TroveManager.sol
+
+echo
+echo
+echo "************************************************************"
+echo "DIFF: Unipool (live) to (ew)"
+echo "************************************************************"
+diff Liquity-Live/Unipool.sol ../contracts/LPRewards/Unipool.sol
